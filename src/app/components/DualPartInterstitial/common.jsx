0e1000c0c2acb06e7ad794eee6e594b690864c6a
Shawn Krisman <shawn.krisman@reddit.com>
2016-12-07 16:56:45 -0800

    Add support for 7 new interstitial variants.

---
+++ src/app/components/DualPartInterstitial/common.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/jsx-curly-spacing */
+
+import React from 'react';
+import { createStructuredSelector } from 'reselect';
+
+import { getDevice } from 'lib/getDeviceFromState';
+import { getBranchLink } from 'lib/smartBannerState';
+
+import DualPartInterstitialHeader from 'app/components/DualPartInterstitial/Header';
+import DualPartInterstitialFooter from 'app/components/DualPartInterstitial/Footer';
+
+const T = React.PropTypes;
+
+export function DualPartInterstitialCommon(props) {
+  return (
+    <div className='DualPartInterstitial__common'>
+      <DualPartInterstitialHeader { ...props } />
+      <DualPartInterstitialFooter { ...props }/>
+    </div>
+  );
+}
+
+DualPartInterstitialCommon.propTypes = {
+  urls: T.array.isRequired,
+  onClose: T.func,
+};
+
+function getUrls(state) {
+  return [
+    getBranchLink(state, {
+      feature: 'interstitial',
+      campaign: 'xpromo_interstitial_listing',
+      utm_medium: 'interstitial',
+      utm_name: 'xpromo_interstitial_listing',
+      utm_content: 'element_1',
+    }),
+    getBranchLink(state, {
+      feature: 'interstitial',
+      campaign: 'xpromo_interstitial_listing',
+      utm_medium: 'interstitial',
+      utm_name: 'xpromo_interstitial_listing',
+      utm_content: 'element_2',
+    }),
+  ];
+}
+
+export const selector = createStructuredSelector({
+  urls: getUrls,
+  device: getDevice,
+});
